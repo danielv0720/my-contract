e49e775dca88e8dde2321f0690e9679451d43611
danielv0720 <alejovelez@nebular.co>
2021-10-10 16:23:30 -0500

    avance daniel

--- src/components/users/UserCard.js
+++ src/components/users/UserCard.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-export const UserCard = ({first_name, email}) => {
+export const UserCard = ({first_name, email, avatar}) => {
   return (
-    <div className="card">
-      {/* <img src="..." className="card-img-top" alt="..."> */}
+    <div className="card m-2 mb-3"style={{width: "18rem"}} >
+      <img src={avatar} className="card-img-top mt-2" />
       <div className="card-body">
         <h5 className="card-title">{first_name}</h5>
         <p className="card-text">{email}</p>
-        {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+        <NavLink 
+            
+              className="btn btn-primary" 
+              exact
+              to="/questions">
+                  Detail
+        </NavLink>
       </div>
     </div>
   )
